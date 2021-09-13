2c0bdef8a74f33ee50b4e9ac37dea1c274423450
Duncan Forssman <duncan.forssman@dimensiondata.com>
2021-09-13 17:48:23 +0200

    Added searching, filtering, paging and sorting functionality Added CORS header for front-end

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using AutoMapper;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -31,32 +32,51 @@ namespace API.Controllers
             _productBrandsRepo = productBrandRepo;
             _productTypesRepo = productTypeRepo;
         }
-
+        //GET PRODUCTS
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams)
+            //if we don't specify [FromQuery] here, it will look to the request body and not the url string
         {
-            var spec = new ProductsWithTypesAndBrandsSpecifcation();
+            var spec = new ProductsWithTypesAndBrandsSpecifcation(productParams);
+
+            var countSpec = new ProductWithFilterForCountSpecification(productParams);
+
+            var totalItems = await _productsRepo.CountAsync(countSpec);
+
             var products = await _productsRepo.ListAsync(spec); //fetch from db
-            return Ok(_mapper.
-                Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>> (products));
+
+            var data = _mapper
+                .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,productParams.PageSize, totalItems, data));
+                
         }
+
+        //GET PRODUCT BY ID
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecifcation(id);
+
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
             if(product == null) {
+
                 return NotFound(new ApiResponse(404));
+
             }
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
+        // GET PRODUCT BRANDS
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok (await _productBrandsRepo.ListAllAsync());
         }
+
+        //GET PRODUCT TYPES
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -15,6 +15,18 @@ namespace Infrastructure.Data
             {
                 query = query.Where(spec.Criteria); // e.g p => p.ProductTypeId == id
             }
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy); // e.g p => p.ProductTypeId == id
+            }
+            if (spec.OrderByDesc != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDesc); // e.g p => p.ProductTypeId == id
+            }
+            if (spec.isPaging) //order matters here, we only want to page after filtering
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take); // e.g p => p.ProductTypeId == id
+            }
 
             query = spec.Includes.Aggregate(query, (current, include)=> current.Include(include));
 
